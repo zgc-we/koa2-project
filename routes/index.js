74fb9a30d362c56538e7c96d3d87ae19cd16f571
ludan <ludan1107743896@qq.com>
2019-09-06 14:44:32 +0800

    cookie 的引入导出

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,20 @@
 const router = require('koa-router')()
 
+router.prefix('/koa'); // 默认加上前缀
+
 router.get('/', async (ctx, next) => {
+  ctx.cookies.set('name', "zhangsan"); // 创建 cookie 
   await ctx.render('index', {
     title: 'Hello Koa 2!'
   })
 })
 
-router.get('/string', async (ctx, next) => {
-  ctx.body = 'koa2 string'
+router.get('/string', async (ctx, next) => { // 字符串类型
+  let _cookies = ctx.cookies.get("name"); // 读取cookies
+  ctx.body = `koa2 string。 这是cookie引入${_cookies}`
 })
 
-router.get('/json', async (ctx, next) => {
+router.get('/json', async (ctx, next) => { // json类型
   ctx.body = {
     title: 'koa2 json'
   }
