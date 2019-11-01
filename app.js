32fad570791245d694a939e5ef63247b14d44e1d
GongXiaoZhu <1107743896@qq.com>
2019-11-01 15:36:53 +0800

    爬虫

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const views = require('koa-views')
 const json = require('koa-json')
 const onerror = require('koa-onerror')
 const bodyparser = require('koa-bodyparser')
-const logger = require('koa-logger')
+const logger = require('koa-logger');
 
 const index = require('./routes/index')
 const users = require('./routes/users')
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,45 @@
 const router = require('koa-router')()
+const cheerio = require('cheerio');
+const charset = require('superagent-charset');
+const superagent = charset(require('superagent'));
 
-router.prefix('/users')
+// router.prefix('/users')
 
-router.get('/', function (ctx, next) {
-  ctx.body = 'this is a users response!'
+let arr;
+
+router.get('/users', function (ctx, next) {
+  url = 'http://shop.bytravel.cn/produce/index226.html'; //target地址
+  superagent.get(url)
+    .charset('gbk')  // 当前页面编码格式
+    .buffer(true)
+    .end((err, data) => { //页面获取到的数据
+      if (err) {
+        // return next(err); 
+        console.log('页面不存在', err)
+      }
+      let html = data.text,
+        $ = cheerio.load(html, {
+          decodeEntities: false,
+          ignoreWhitespace: false,
+          xmlMode: false,
+          lowerCaseTags: false
+        }), //用cheerio解析页面数据
+        obj = {};
+        arr = [];
+      // cheerio的使用类似jquery的操作
+      $("table tbody").each((index, element) => {
+        let $element = $(element);
+        $element.find('#tctitle').next().find('a').addClass('link').attr('class', 'link').text('')
+        arr.push({
+          'title': $element.find('a.blue14b').text(),
+          'image': $element.find('#bright img').attr('src'),
+          'summary': $element.find('#tctitle').next().text(),
+          'is_cgiia': $element.find('#tctitle font').attr('color') === 'green' ? 1 : 0
+        })
+      })
+    })
+    ctx.body = arr
+  // console.log(arr)
 })
 
 router.get('/bar', function (ctx, next) {
